9810611943c5bb69b3218e23eb5381e89eb57c9c
Antonio L <256megas@gmail.com>
2022-02-08 11:14:48 +0100

    first commit

---
+++ src/Controller/NoticiasController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NoticiasController extends AbstractController
+{
+    #[Route('/noticias', name: 'noticias')]
+    public function index(): Response
+    {
+        $noticias = [
+            [
+                'ID' => 1,
+                'titulo'=> "Esta es la noticia UNO"
+            ],
+            [
+                'ID' => 2,
+                'titulo'=> "Esta es la noticia DOS"
+            ],
+            [
+                'ID' => 3,
+                'titulo'=> "Esta es la noticia TRES"
+            ]
+            
+            ];
+        return $this->render('noticias/index.html.twig', [
+            'noticias' => $noticias,
+        ]);
+    }
+
+
+    #[Route('/noticia/{id}', name: 'noticiaSingle',defaults:["id"=> 1] )]
+    public function noticiaSingle($id): Response
+    {
+        return $this->render('noticias/single.html.twig', [
+            'idNoticia' => $id,
+        ]);
+    }
+}
